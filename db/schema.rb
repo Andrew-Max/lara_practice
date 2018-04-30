d3f216e7c8de1eaa630ffa21716ed6c8c6a736d7
Lara Corkrey <laracorkrey@gmail.com>
2018-04-29 20:37:46 -0700

    updated routes, views, etc

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180409043022) do
+ActiveRecord::Schema.define(version: 20180419050644) do
+
+  create_table "messages", force: :cascade do |t|
+    t.string "body"
+    t.string "title"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
