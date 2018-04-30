d9083d206b76fa26cad1ff885b5275f52baf4951
Lara Corkrey <laracorkrey@gmail.com>
2018-04-29 21:41:27 -0700

    added new users route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,27 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  #get /users/new
+  def new
+    @user = User.new
+  end
+
+  #post /users
+  def create
+    user = User.create(user_params)
+    redirect_to user_path(user)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,6 @@ Rails.application.routes.draw do
 
   resources 'users'
 
-  # get all of  the comments for the user where user_id = :id,
-  get '/users/:id/comments', to: 'comments#index'
 
 
-  get '/places', to: 'places#index'
-  get '/places/:id', to: 'places#show'
-  get '/places/new', to: 'places#new'
-  post '/places/', to: 'places#create'
-  get '/places/:id/edit', to: 'places#edit'
-  put '/places/:id', to: 'places#update'
-  delete '/places/:id', to: 'places#destroy'
-
-  get '/xyzs', to: 'xyzs#index'
-  get '/xyzs/:id', to: 'xyzs#show'
 end
